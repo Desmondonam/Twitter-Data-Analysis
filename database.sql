43dd71851e8d7034478bb8e6a6624a4b02e4166b
Desmond Onam <desmondonam@gmail.com>
2021-06-29 15:45:52 +0300

    Message

--- database.sql
+++ database.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE twitter;
+-- use database
+USE [twitter];
+GO 
+-- check to see if table exist in sys.tables ignore DROP TABLE if it does not 
+IF EXISTS (SELECT *
+	FROM sys.tables
+    WHERE SCHEMA_NAME(schema_id) like 'dbo'
+    AND name like 'MyTable0')
+DROP TABLE [dbo].[MyTable0];
+GO
