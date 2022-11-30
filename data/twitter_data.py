a2d9ee30f7258d7a7d97a0b265e55040d29e1d50
Desmondonam <desmondonam@gmail.com>
2022-11-30 10:10:15 +0300

    change the data in twitter data file

--- data/twitter_data.py
+++ data/twitter_data.py
@@ -7,7 +7,7 @@ from tweepy import Cursor
 
 import pandas as pd
 import json
-import csv 
+import csv
 
 # to view all columns
 pd.set_option("display.max.columns", None)
@@ -23,14 +23,14 @@ auth.set_access_token(accessToken, accessTokenSecret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 # Commented out IPython magic to ensure Python compatibility.
 # %cd /content/drive/My Drive/Batch5_Twitter_Data
 
-hashtags=["#inflation", "#fuelprice", "#fuelpricehike", "#fuelprices", "#fuelshortage", "#foodprice", "#oilprice", "#oilprices", "#cookingoilprice",
-          "#unemployment", "#unemploymentrate", "#economiccrisis", "#economichardship"]
+hashtags = ["#inflation", "#fuelprice", "#fuelpricehike", "#fuelprices", "#fuelshortage", "#foodprice", "#oilprice", "#oilprices", "#cookingoilprice",
+            "#unemployment", "#unemploymentrate", "#economiccrisis", "#economichardship"]
 
 # kenya, geocode="0.0236,37.9062,530km"
 # south africa, geocode="-26.22081,28.03239,400km"
@@ -39,14 +39,15 @@ hashtags=["#inflation", "#fuelprice", "#fuelpricehike", "#fuelprices", "#fuelsho
 # Add gecode variable for country specific data and remove it for general data.
 names = []
 for hashtag in hashtags:
-    tweets = tweepy.Cursor(api.search, q = hashtag, wait_on_rate_limit=True).items(500) #iterating through 500 tweets for each hashtag
+    tweets = tweepy.Cursor(api.search, q=hashtag, wait_on_rate_limit=True).items(
+        500)  # iterating through 500 tweets for each hashtag
     users = []
     for status in tweets:
         name = status.user.screen_name
-        t=status.text
+        t = status.text
         users.append(name)
     names.append(users)
-    
+
 screen_names = [y for x in names for y in x]
 
 # merge the lists
@@ -57,21 +58,24 @@ print(len(names))
 screen_names = list(dict.fromkeys(names))
 print(len(screen_names))
 
-df = pd.DataFrame({'names':screen_names})
+df = pd.DataFrame({'names': screen_names})
 df.to_csv('data/Economic_Twitter_Data.csv')
 df.shape
 
-#Mining 500 tweets per user since january 2022
+# Mining 500 tweets per user since january 2022
+
 
 def get_tweets(screen_names):
-  for name in screen_names:
-    #Saving the tweets to a json file. The name of the file changes depending on the data being collected
+    for name in screen_names:
+        # Saving the tweets to a json file. The name of the file changes depending on the data being collected
 
-    with open("data/Economic_Twitter_Data.json", 'a') as f:     #open file first
-      tweets = tweepy.Cursor(api.user_timeline, screen_name = name, since = '2022-01-01', wait_on_rate_limit=True).items(500)
-      for tweet in tweets:
-        json.dump(tweet._json, f)  #dump each to file, f
-        f.write("\n")
-  return print('Done. The keys are: ', list(tweet._json.keys()))
+        with open("data/Economic_Twitter_Data.json", 'a') as f:  # open file first
+            tweets = tweepy.Cursor(api.user_timeline, screen_name=name,
+                                   since='2022-01-01', wait_on_rate_limit=True).items(500)
+            for tweet in tweets:
+                json.dump(tweet._json, f)  # dump each to file, f
+                f.write("\n")
+    return print('Done. The keys are: ', list(tweet._json.keys()))
+
 
 get_tweets(screen_names[0:111])
